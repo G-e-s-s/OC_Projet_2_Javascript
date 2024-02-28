d0513cdfb682190644123d181fd716780780f666
Gessica Goublet <gess_06570@hotmail.fr>
2024-02-28 11:07:41 +0100

    Finalisation

--- FrontEnd/scriptsLogin.js
+++ FrontEnd/scriptsLogin.js
@@ -1,27 +1,27 @@
-let form = document.querySelector("form");
+let form = document.querySelector("form"); //document.getElementById("btnLogin")
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const login = {
         email: event.target.querySelector("[name=email]").value,
         password: event.target.querySelector("[name=password]").value,
-    };
-    const loginContainer = JSON.stringify(login);
+    }; // Recupère les valeurs saisies par l'utilisateur
+    const loginContainer = JSON.stringify(login); // Convertion de l'objet de login au format JSON (pour l'envoi)
     const reponse = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: loginContainer,
-    });
+        headers: { "Content-Type": "application/json" }, // Precise le type d'information de ce qui est envoyé
+        body: loginContainer, // Information envoyée
+    }); // Envoi des identifiant aux back-end
     if (reponse.ok){
         const value = await reponse.json();
-        window.localStorage.setItem("token", value["token"]);
-        window.location = "index.html";
+        window.localStorage.setItem("token", value["token"]); // On stock le token en local
+        window.location = "index.html"; // Redirection sur la page d'accueil après connexion
     }
     else{
         if (reponse.status === 401) {
             let erreur = document.getElementById("messageErreur");
             erreur.innerHTML = "E-mail ou mot de passe introuvable";
-        } else{
+        }else{
             let erreur = document.getElementById("messageErreur");
             erreur.innerHTML = "E-mail ou mot de passe introuvable";
         }
