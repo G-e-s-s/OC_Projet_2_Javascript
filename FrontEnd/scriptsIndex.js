fbe4ce7ec9d82989e02d694a9f6f74939a23c578
Gessica Goublet <gess_06570@hotmail.fr>
2024-02-28 11:07:07 +0100

    Finalisation

--- FrontEnd/scriptsIndex.js
+++ FrontEnd/scriptsIndex.js
@@ -1,7 +1,6 @@
 const url = "http://localhost:5678/api/";
 const galleryContainer = document.getElementById("gallery");
 const filtersContainer = document.getElementById("filters");
-window.localStorage.getItem("token");
 
 //Filtres//
 async function getFilters () {
@@ -10,15 +9,19 @@ async function getFilters () {
     categories.unshift({
         id : 0,
         name : "Tous"
-    });
+    }); // Rajout au début de la categorie "Tous"
     for(i in categories) {
+            /*<button id="all">Tous</button> 
+            <button id="object">Objets</button> 
+            <button id="apartment">Appartements</button> 
+            <button id="hotel">Hôtels & restaurants</button>*/
         let button = document.createElement("button");
         button.innerHTML = categories[i].name;
         button.setAttribute("data-id", categories[i].id);
         button.addEventListener("click", (event) => {
             event.preventDefault();
             getArticles(parseInt(event.target.getAttribute("data-id")));
-        });
+        }); 
         filtersContainer.appendChild(button);
     }
 };
@@ -26,11 +29,15 @@ getFilters();
 
 //Images projets//
 async function getArticles (categorieNum) {
-    const reponse = await fetch(url + "works");
-    const works = await reponse.json();
-    galleryContainer.innerHTML = "";
+    const reponse = await fetch(url + "works"); // Recuperation des images disponibles dans le back-end
+    const works = await reponse.json(); // Passage du format JSON à un tableau JS
+    galleryContainer.innerHTML = ""; // Enlever les anciennes photos afficher
     for(i in works) {
         if( (works[i].categoryId === categorieNum) || (categorieNum === 0)) {
+                /*<figure>
+                    <img src="assets/images/abajour-tahina.png" alt="Abat-jour Tahina">
+                    <figcaption>Abat-jour Tahina</figcaption>
+                </figure>*/
             let figure = document.createElement("figure");
             let img = document.createElement("img");
             let figcaption = document.createElement("figcaption");
@@ -49,6 +56,8 @@ getArticles(0);
 const token = window.localStorage.getItem("token");
 if (token !== null) {
     //Black Bar//
+        /*<img id="iconeEdition" src="./assets/icons/editer.png" alt="Editer">
+        <span id="btnEdition">Mode édition</span>*/
     const blackBar = document.getElementById("blackBar");
     let img = document.createElement("img");
     img.setAttribute("id", "iconeEdition");
@@ -62,16 +71,19 @@ if (token !== null) {
     blackBar.style.display = "flex";
 
     //Log In -> Log Out//
+        /*<li><a id="log" href="login.html">login</a></li>*/
     const logOut = document.getElementById("log");
-    logOut.innerHTML = "Logout";
+    logOut.innerHTML = "Logout"; // Log in -> Log Out
     logOut.setAttribute("href", "");
-    logOut.addEventListener("click", (event) => {
+    logOut.addEventListener("click", (event) => { // Se déconnecter
         event.preventDefault();
-        localStorage.removeItem("token");
-        location.reload();
+        localStorage.removeItem("token"); //Supprime le token en local
+        location.reload(); //Rachargement de la page
     });
 
     //Bouton modifier//
+        /*<img id="iconeModification" src="./assets/icons/modifications.png" alt="Modifier">
+		<button id="btnModification">Modifier</button>*/
     const projets = document.getElementById("projets");
     let imgProjets = document.createElement("img");
     imgProjets.setAttribute("id", "iconeModification");
@@ -86,10 +98,11 @@ if (token !== null) {
 
     //Cacher les filtres//
     const filters = document.getElementById("filters");
-    filters.style.display='none';
+    filters.style.display="none"; // Propriété CSS pour cacher les filtres
 };
 
-//Modale Modifier image//
+
+//Modale1 : Supprimer image//
 async function clickBtnModifier() {
     const modifierPhoto = document.getElementById("btnModification");
 
@@ -106,21 +119,28 @@ async function modaleSuppImg() {
     aside.setAttribute("role", "dialog");
     aside.setAttribute("aria-labelledby", "titleModal");
 
+    //Elements de la modale 1//
     const popupList = document.createElement("div");
     popupList.setAttribute("id", "popupList");
 
+    //Bouton fermer modale//
     const buttonClose = document.createElement("button");
     buttonClose.setAttribute("id", "close");
     const iconeClose = document.createElement("i");
     iconeClose.setAttribute("class", "fa-solid fa-xmark");
     buttonClose.addEventListener("click", async () => {
-        document.getElementById("modal-list").remove();
+        document.getElementById("modal-list").remove(); //Retire l'élément du DOM
     });
 
+    //Afficher les images//
     const h1Galery = document.createElement("h1");
     h1Galery.setAttribute("class", "titlePopUp");
     h1Galery.innerHTML = "Galerie photo";
 
+        /*<div class="imgGalery">
+            <i class="fa-solid fa-trash-can iconeGalery"></i>
+            <img src="assets/images/abajour-tahina.png" alt="Abat-jour Tahina">
+        </div>*/
     const imagesProjet = document.createElement("div");
     imagesProjet.setAttribute("id", "images");
     imagesProjet.setAttribute("class", "scroller");
@@ -144,24 +164,32 @@ async function modaleSuppImg() {
                         "Authorization" : "Bearer " + window.localStorage.getItem("token"),
                     }
                 }
-            );
-            document.getElementById("gallery").innerHTML = "";
-            getArticles(0);
-            document.getElementById("modal-list").remove();
-            modaleSuppImg();
+            ).then((response) => { 
+                if (response.ok) {
+                    document.getElementById("gallery").innerHTML = "";
+                    getArticles(0);
+                    document.getElementById("modal-list").remove();
+                    modaleSuppImg();
+                }
+            });
+            
         });
         imagesProjet.appendChild(imgGalery);
         imgGalery.appendChild(iconeGalery);
         imgGalery.appendChild(img);
     };
 
+    //Bar de séparation//
     const hr = document.createElement("hr");
 
+    //Bouton ajouter photo//
+        /*<input id="addImage" type="submit" value="Ajouter une photo">*/
     const inputAddImg = document.createElement("input");
     inputAddImg.setAttribute("id", "addImage");
     inputAddImg.setAttribute("type", "submit");
     inputAddImg.setAttribute("value", "Ajouter une photo");
 
+    //Parents//
     modale.appendChild(aside);
     aside.appendChild(popupList);
 
@@ -175,16 +203,16 @@ async function modaleSuppImg() {
 
     modale.style.display = "flex";
 
+    //Renvoie vers la 2ème modale//
     inputAddImg.addEventListener("click", async () =>{
-        document.getElementById("modal-list").remove();
+        document.getElementById("modal-list").remove(); //Permet de garder une seule modale ouverte
         modalAddImage();
     });
 
 };
 
-//Modale Ajouter image//
+//Modale2 : Ajouter image//
 async function modalAddImage(){
-
     const modale = document.getElementById("modale");
 
     const aside = document.createElement("aside");
@@ -193,9 +221,15 @@ async function modalAddImage(){
     aside.setAttribute("role", "dialog");
     aside.setAttribute("aria-labelledby", "titleModal");
 
+    //Elements de la modale 2//
     const popupAdd = document.createElement("div");
     popupAdd.setAttribute("id", "popupAdd");
 
+    //Bouton retour et croix pour fermer modale//
+        /*<div id="return">
+            <button id="returnModal"><i class="fa-solid fa-arrow-left"></i></button>
+            <button id="closeModal"><i class="fa-solid fa-xmark"></i></button>
+        </div>*/
     const back = document.createElement("div");
     back.setAttribute("id", "return");
         const returnModal = document.createElement("button");
@@ -214,13 +248,26 @@ async function modalAddImage(){
             const iconeClose = document.createElement("i");
             iconeClose.setAttribute("class", "fa-solid fa-xmark");
 
+    //Titre//
     const h1AjoutImg = document.createElement("h1");
     h1AjoutImg.setAttribute("class", "titlePopUp");
     h1AjoutImg.innerHTML = "Ajout photo";
 
+    //Ajouter photo//
     const ajoutPhoto = document.createElement("div");
     ajoutPhoto.setAttribute("id", "ajoutPhoto");
 
+    //Télécharger l'image à ajouter//
+            /*<div id="element">
+                <div id="imageEnCours">
+                    <img src= ""><i id="iconeImage" class="fa-regular fa-image"></i>
+                </div>
+				<button id="classImage">
+                    label for ="btnAddImage" id="btnFile">+ Ajouter photo</label>
+                    <input id="btnAddImage" type="file" accept = ".jpg,.png"/>
+				</button>
+				<p id="textInfo">jpg, png : 4mo max</p>
+			</div>*/
     const element = document.createElement("div");
     element.setAttribute("id", "element");
         const iconeImage = document.createElement("i");
@@ -258,6 +305,9 @@ async function modalAddImage(){
             }
         });
 
+    //Ajouter le titre de l'image//
+        /*<label for="title" id="label">Title</label>
+		<input type="text" id="namePhoto" name="title" required>*/
     const label = document.createElement("label");
     label.setAttribute("for", "title");
     label.setAttribute("id", "label");
@@ -270,6 +320,15 @@ async function modalAddImage(){
     namePhoto.setAttribute("required",true);
     namePhoto.addEventListener("input",valider);
 
+    //Ajouter catégorie de l'image en dynamique//
+        /*<form method="get" action="">
+            <p>
+                <label id="categorie" for="title">Catégorie</label>
+                <select name="categorie" id="nameCategorie">
+                    <option value=""></option>
+                </select>
+            </p>
+		</form>*/
     const form = document.createElement("form");
     form.setAttribute("method", "get");
     form.setAttribute("action", "");
@@ -295,12 +354,15 @@ async function modalAddImage(){
                 option.innerHTML = categories[i].name;
                 nameCategorie.appendChild(option);
             }
-        
+    //Message erreur si formulaire mal rempli//
     const messageErreur = document.createElement("span");
     messageErreur.setAttribute("id", "messageErreur")
     
+    //Bar de séparation//
     const hr = document.createElement("hr");
 
+    //Bouton enregistrer nouvelle image//
+        /*<input id="validerImage" type="submit" value="Valider">*/
     const validerImage = document.createElement("input");
     validerImage.setAttribute("id", "validerImage");
     validerImage.setAttribute("type", "submit");
@@ -335,7 +397,8 @@ async function modalAddImage(){
             erreur.innerHTML = "Le formulaire n’est pas correctement rempli";
         }
     });
-        
+    
+    //Parents//
     modale.appendChild(aside);
     aside.appendChild(popupAdd);
 
@@ -370,14 +433,17 @@ async function modalAddImage(){
     popupAdd.style.display = "flex";
 };
 
+//Bouton "valider image" si formulaire bien rempli//
 function valider(){
     file = document.getElementById("btnAddImage");
     title = document.getElementById("namePhoto");
-    cat = document.getElementById("nameCategorie");
+    categorie = document.getElementById("nameCategorie");
     validAjout = document.getElementById("validerImage");
-    if((file != "") && (title.value != "") && (cat.value != "")){
+    if((file != "") && (title.value != "") && (categorie.value != "")){ //Afficher le bouton en vert si tout est correctement rempli
+        //validAjout.disabled = false;
         validAjout.style.backgroundColor = "rgba(29, 97, 84, 1)";
     } else {
+        //validAjout.disabled = true;
         validAjout.style.backgroundColor = "rgba(167, 167, 167, 1)";
     }
 };
